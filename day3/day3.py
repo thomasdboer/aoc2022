cd574a451375b6cc99b9dba6f700ca91c9f5e4eb
Thomas den Boer <thomas@denboermail.nl>
2022-12-05 15:23:10 +0100

    finish day3

---
+++ day3/day3.py
@@ -0,0 +1,41 @@
+import os
+import string
+filename = os.path.join(os.path.dirname(__file__), "input.txt")
+f = open(filename, "r")
+input = f.read().splitlines()
+f.close()
+
+def matchcompartments(comp1, comp2):
+    for char in comp1:
+        if char in comp2:
+            return char
+
+duplicates = []
+for rucksack in input:
+    slice = int(len(rucksack)/2)
+    compartment1 = rucksack[:slice]
+    compartment2 = rucksack[slice:]
+    duplicates.append(matchcompartments(compartment1, compartment2))
+
+valueMap = {char:val for (val, char) in enumerate(string.ascii_letters, start=1)}
+
+def calculate_priority(characters):
+    sum_priority = 0
+    for char in characters:
+        sum_priority += valueMap[char]
+    return sum_priority
+
+# Part A solution
+print("Solution Part A:", calculate_priority(duplicates))
+
+# Part B
+groups = [input[i:i+3] for i in range(0, len(input), 3)]
+badges = []
+for group in groups:
+    badge = ""
+    for char in group[0]:
+        if char in group[1] and char in group[2]:
+            badge = char
+    badges.append(badge)
+
+print("Solution Part B:", calculate_priority(badges))
