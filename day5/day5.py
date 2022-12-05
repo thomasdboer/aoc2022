e92ef8cfc539aca0280c1c573f995ed04cc0b86f
Thomas den Boer <thomas@denboermail.nl>
2022-12-05 19:10:57 +0100

    Finished day5 part2

--- day5/day5.py
+++ day5/day5.py
@@ -6,10 +6,14 @@ f.close()
 
 input_stacks = input[0:8]
 instructions = input[10:]
-stacks = list([] for i in range(1,10))
-instructions_short = []
+
 
 def parse_instructions():
+    global stacks
+    global instructions_short
+    instructions_short = []
+    stacks = list([] for i in range(1, 10))
+
     # Flip the input stacks into 2D list
     for row in input_stacks:
         blocks = [row[i:i+4] for i in range(0, len(row)+1, 4)]
@@ -20,15 +24,19 @@ def parse_instructions():
         parsed_inst = [int(x) for x in instruction.split() if x.isdigit()]
         instructions_short.append(parsed_inst)
 
+
 def move_blocks(n, start, stop):
+
     # Pop blocks from original stack
     selected_stack = stacks[start-1]
     blocks_in_transit = [selected_stack.pop() for block in selected_stack[-n:]]
+
     # Append to new stack
     for block in blocks_in_transit:
         stacks[stop-1].append(block)
 
-def solveA():
+
+def solve_a():
     parse_instructions()
     for n, start, stop in instructions_short:
         move_blocks(n, start, stop)
@@ -37,5 +45,30 @@ def solveA():
         solution += stack[-1]
     return solution
 
-print("Part A solution:", solveA())
 
+print("Part A solution:", solve_a())
+
+
+def batch_move_blocks(n, start, stop):
+
+    # Pop blocks from original stack
+    selected_stack = stacks[start-1]
+    blocks_in_transit = [selected_stack.pop() for block in selected_stack[-n:]]
+
+    # Append to new stack in reverse order
+    blocks_in_transit.reverse()
+    for block in blocks_in_transit:
+        stacks[stop-1].append(block)
+
+
+def solve_b():
+    parse_instructions()
+    print(stacks, instructions_short)
+    for n, start, stop in instructions_short:
+        batch_move_blocks(n, start, stop)
+    solution = ''
+    for stack in stacks:
+        solution += stack[-1]
+    return solution
+
+print("Part B solution:", solve_b())
