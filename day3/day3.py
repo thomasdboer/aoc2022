1e16a853ebb198a217c3e5e38ef3e80e4e5da5ac
Thomas den Boer <thomas@denboermail.nl>
2022-12-05 20:09:29 +0100

    Refactor day4 & day3

--- day3/day3.py
+++ day3/day3.py
@@ -5,23 +5,27 @@ f = open(filename, "r")
 input = f.read().splitlines()
 f.close()
 
+# Value function
+valueMap = {char: val for (val, char) in enumerate(
+    string.ascii_letters, start=1)}
+
+
+# Parse input
+def parse_input():
+    global duplicates
+    duplicates = []
+    for rucksack in input:
+        slice = int(len(rucksack)/2)
+        compartment1 = rucksack[:slice]
+        compartment2 = rucksack[slice:]
+        duplicates.append(matchcompartments(compartment1, compartment2))
+
 # Helper function
 def matchcompartments(comp1, comp2):
     for char in comp1:
         if char in comp2:
             return char
-
-# Parse input
-duplicates = []
-for rucksack in input:
-    slice = int(len(rucksack)/2)
-    compartment1 = rucksack[:slice]
-    compartment2 = rucksack[slice:]
-    duplicates.append(matchcompartments(compartment1, compartment2))
-
-valueMap = {char: val for (val, char) in enumerate(
-    string.ascii_letters, start=1)}
-
+            
 # Solve
 def calculate_priority(characters):
     sum_priority = 0
@@ -29,18 +33,29 @@ def calculate_priority(characters):
         sum_priority += valueMap[char]
     return sum_priority
 
+def solve_a():
+    parse_input()
+    return calculate_priority(duplicates)
+
 
 # Part A solution
-print("Solution Part A:", calculate_priority(duplicates))
+print("Solution Part A:", solve_a())
 
 # Part B
-groups = [input[i:i+3] for i in range(0, len(input), 3)]
-badges = []
-for group in groups:
-    badge = ""
-    for char in group[0]:
-        if char in group[1] and char in group[2]:
-            badge = char
-    badges.append(badge)
+def find_badges():
+    groups = [input[i:i+3] for i in range(0, len(input), 3)]
+    badges = []
+    for group in groups:
+        badge = ""
+        for char in group[0]:
+            if char in group[1] and char in group[2]:
+                badge = char
+        badges.append(badge)
+    return badges
 
-print("Solution Part B:", calculate_priority(badges))
+def solve_b():
+    parse_input()
+    badges = find_badges()
+    return calculate_priority(badges)
+
+print("Solution Part B:", solve_b())
--- day4/day4.py
+++ day4/day4.py
@@ -5,49 +5,57 @@ input = f.read().splitlines()
 f.close()
 
 # Parse input
-assignments = []
-for pair in input:
-    section_range = []
-    elf_assignment = pair.split(',')
-    for sections in elf_assignment:
-        (x, y) = sections.split('-')
-        section_range.append((int(x), int(y)))
-    assignments.append(section_range)
+def parse_inputs():
+    global assignments
+    assignments = []
+    for pair in input:
+        section_range = []
+        elf_assignment = pair.split(',')
+        for sections in elf_assignment:
+            (x, y) = sections.split('-')
+            section_range.append((int(x), int(y)))
+        assignments.append(section_range)
+
+# Helper function
+def get_min_max(assignment):
+    min_a = assignment[0][0]
+    max_a = assignment[0][1]
+    min_b = assignment[1][0]
+    max_b = assignment[1][1]
+    return min_a, max_a, min_b, max_b
 
 # Helper function
 def range_encapsulates(xmin, xmax, ymin, ymax):
     return (xmin <= ymin and xmax >= ymax) or (ymin <= xmin and ymax >= xmax)
 
 # Solve
-counter_a = 0
-for assignment in assignments:
-    min_a = assignment[0][0]
-    max_a = assignment[0][1]
-    min_b = assignment[1][0]
-    max_b = assignment[1][1]
-    if range_encapsulates(min_a, max_a, min_b, max_b):
-        counter_a += 1
+def solve_a():
+    parse_inputs()
+    counter = 0
+    for assignment in assignments:
+        min_a, max_a, min_b, max_b = get_min_max(assignment)
+        if range_encapsulates(min_a, max_a, min_b, max_b):
+            counter += 1
+    return counter
 
 # Solution Part A
-print("Solution Part A:", counter_a)
+print("Solution Part A:", solve_a())
 
 # Part B
-# All pairs that have any overlap at all
-
 # Helper function
 def range_overlaps(xmin, xmax, ymin, ymax):
     x, y = set(range(xmin, xmax+1)), set(range(ymin, ymax+1))
     return bool(x.intersection(y))
 
 # Solve
-counter_b = 0
-for assignment in assignments:
-    min_a = assignment[0][0]
-    max_a = assignment[0][1]
-    min_b = assignment[1][0]
-    max_b = assignment[1][1]
-    if range_overlaps(min_a, max_a, min_b, max_b):
-        counter_b += 1
+def solve_b():
+    parse_inputs()
+    counter = 0
+    for assignment in assignments:
+        min_a, max_a, min_b, max_b = get_min_max(assignment)
+        if range_overlaps(min_a, max_a, min_b, max_b):
+            counter += 1
+    return counter
 
 # Solution part B
-print("Solution Part B:", counter_b)
+print("Solution Part B:", solve_b())
