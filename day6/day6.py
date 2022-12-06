f80f0222f36970713eae436be2a79dcb4e590094
Thomas den Boer <thomas@denboermail.nl>
2022-12-06 14:19:37 +0100

    Finish day6

--- day6/day6.py
+++ day6/day6.py
@@ -1,45 +1,55 @@
 import os
 from time import perf_counter
-
 t1_start = perf_counter()
 filename = os.path.join(os.path.dirname(__file__), "input.txt")
-f = open(filename, "r")
+
 
 markers = []
 
 
 def markindex(idx):
-    markers.append(idx)
+    return (idx)
 
 
-def is_marker(buffer):
-    marker_packet = True
-    for idx, char in enumerate(buffer):
-        if buffer[idx+1:].__contains__(char) or len(buffer) < 4:
-            marker_packet = False
-    return marker_packet
+def is_marker(buffer, size):
+    if len(set(buffer)) == size:
+        return True
 
 
-def read_input_stream():
+def read_input():
+    global input
+    f = open(filename, "r")
     input = f.read().rstrip('\n')
     f.close()
-    buffer = []
-    for idx, i in enumerate(input):
-        # Use insert() and pop() for O(1) time
-        buffer.insert(0, i)
-        if is_marker(buffer):
-            # Mark index
-            markindex(idx+1)
-
-        if (len(buffer) > 3):
-            buffer.pop()
 
 
 def solve_a():
-    read_input_stream()
-    return markers[0]
+    read_input()
+    buffer = []
+    # O(i)
+    for idx, i in enumerate(input):
+        # insert() and pop() are O(1)
+        buffer.insert(0, i)
+        if len(buffer) > 4:
+            buffer.pop()
+            if is_marker(buffer, 4):
+                return idx+1
 
 
 print("Part A solution: ", solve_a())
 t1_stop = perf_counter()
+
+
+def solve_b():
+    buffer = []
+    for idx, i in enumerate(input):
+        buffer.insert(0, i)
+        if len(buffer) > 14:
+            buffer.pop()
+            if is_marker(buffer, 14):
+                return idx+1
+
+
+print("Part B solution: ", solve_b())
+
 print("Solution reached in: ", t1_stop-t1_start)
