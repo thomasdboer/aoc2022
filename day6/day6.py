2ac0a98c53046f7156c6d86ac31b901526635fe2
Thomas den Boer <thomas@denboermail.nl>
2022-12-22 23:59:22 +0100

    Completed day8

--- day6/day6.py
+++ day6/day6.py
@@ -1,41 +1,50 @@
 import os
+from multiprocessing import Pool
 from time import perf_counter
 t1_start = perf_counter()
-filename = os.path.join(os.path.dirname(__file__), "input.txt")
+filename = os.path.join(os.path.dirname(__file__), "extra.txt")
 
 def read_input():
-    global input
     f = open(filename, "r")
     input = f.read().rstrip('\n')
     f.close()
+    return input
 
 
 def solve_a():
-    read_input()
+    input = read_input()
     buffer = []
     # O(i)
     for idx, i in enumerate(input):
         # insert() and pop() are O(1)
         buffer.insert(0, i)
-        if len(buffer) > 4:
+        if len(buffer) > 94:
             buffer.pop()
-            if len(set(buffer)) == 4:
+            if len(set(buffer)) == 94:
                 return idx+1
 
 
-print("Part A solution: ", solve_a())
+# print("Part A solution: ", solve_a())
 
-
-def solve_b():
+def find_packet(j):    
+    input = read_input()
     buffer = []
     for idx, i in enumerate(input):
         buffer.insert(0, i)
-        if len(buffer) > 14:
+        if len(buffer) > j:
             buffer.pop()
-            if len(set(buffer)) == 14:
-                return idx+1
+            if len(set(buffer)) == j:
+                return idx+1 if idx > 1 else 1
+
+def solve_b():
+    total = 0
+    with Pool() as pool:
+        for result in pool.map(find_packet, range(1,95)):
+            total += result  # type: ignore
+    return total
 
 
-print("Part B solution: ", solve_b())
-t1_stop = perf_counter()
-print("Solution reached in:", round(t1_stop-t1_start, 5), 's')
+if __name__=="__main__":
+    print("Part B solution: ", solve_b())
+    t1_stop = perf_counter()
+    print("Solution reached in:", round(t1_stop-t1_start, 5), 's')
---
+++ day8/day8.py
@@ -0,0 +1,98 @@
+import os
+from time import perf_counter
+import math
+
+t1_start = perf_counter()
+filename = os.path.join(os.path.dirname(__file__), "input.txt")
+
+# Form: {"0" [], "1" [], "2": []}
+
+
+class Grid(object):
+    cells = []
+
+    def __init__(self, grid: list[list[int]]) -> None:
+        self.cells = grid
+
+    # Not related to Tree as a data structure
+    # Cell values refer to trees
+    def get_tree(self, x, y):
+        return self.cells[y][x]
+
+    def dist_y(self, x, y, reverse):
+        height = self.get_tree(x, y)
+        column = [row[x] for row in self.cells[:y]] if reverse else [row[x] for row in self.cells[y+1:]]
+        if reverse: column.reverse()
+        y_dist = next((idx for idx, tree in enumerate(column, 1) if tree >= height), len(column))
+        return y_dist
+
+    def dist_x(self, x, y, reverse):
+        height = self.get_tree(x, y)
+        row = self.cells[y][:x] if reverse else self.cells[y][x+1:]
+        if reverse: row.reverse()
+        x_dist = next((idx for idx, tree in enumerate(row, 1) if tree >= height), len(row))
+        return x_dist
+
+    
+    def get_scenic_score(self, x, y):
+        return [self.dist_x(x, y, True), self.dist_x(x, y, False),
+         self.dist_y(x, y, True), self.dist_y(x, y, False)]
+        
+
+
+def parse_input():
+    grid = []
+    with open(filename) as file:
+        grid = [[int(y) for y in list(x.rstrip())] for x in file.readlines()]
+    return grid
+
+# Only used for part A. Does not use Grid object.
+
+
+def find_visible_trees(input_grid):
+    visibility = []
+
+    for (row_idx, row) in enumerate(input_grid):
+        visibility.append([False] * len(row))
+
+        for (col_idx, column) in enumerate(row):
+            # Search laterally, first left then right
+            visible_x = [x for x in row[:col_idx] if x >= column] == [] or [
+                x for x in row[col_idx+1:] if x >= column] == []
+
+            # Search vertically, first down then up
+            visible_y = [x for x in input_grid[row_idx+1:] if x[col_idx] >= column] == [] or [
+                x for x in input_grid[:row_idx] if x[col_idx] >= column] == []
+
+            visibility[row_idx][col_idx] = visible_x or visible_y
+    return visibility
+
+
+def solve_a():
+    grid = parse_input()
+    visibility = find_visible_trees(grid)
+    visible_row = [row.count(True) for row in visibility]
+    return sum(visible_row)
+
+
+print(solve_a())
+
+
+def solve_b():
+    grid = Grid(parse_input())
+    scoremap = []
+    for (y, row) in enumerate(grid.cells):
+        row_scenicscores = []
+        for (x, tree) in enumerate(row):
+            if 0 < x < len(row) - 1 and 0 < y < len(grid.cells) - 1:
+                scores = grid.get_scenic_score(x, y)
+                treescore = 1
+                for score in scores:
+                    treescore *= score
+            else: treescore = 0
+            row_scenicscores.append(treescore)
+        scoremap.append(row_scenicscores)
+    return max([max(b) for b in [x for x in scoremap]])
+
+
+print(solve_b())
